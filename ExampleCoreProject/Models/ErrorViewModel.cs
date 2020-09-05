9aa553d8cba4a4deb57109457c3f957f8c0a2007
Hasancan Kazan <hasancankazan@gmail.com>
2020-09-05 23:25:39 +0300

    initial commit

---
+++ ExampleCoreProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExampleCoreProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
